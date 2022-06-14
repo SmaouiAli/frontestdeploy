f1e6cea65e946739c678d6b86ce484cd5751e0da
SmaouiAli <smaouiali22@gmail.com>
2022-06-14 14:43:16 +0100

    chnaged server

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import {useEffect,useState} from "react"
 function App() {
   const [data, setData] = useState([]);
   useEffect(() => {
-    fetch("https://deployt-est.herokuapp.com/api/data")
+    fetch("https://test-dep-app-back.herokuapp.com/api")
       .then((res) => res.json())
-      .then((json) => setData(json));
+      .then((json) => setData(json[0]));
   }, []);
   return (<div className="App">
-    <div>value:{data?.results}</div>
+    <div>value:{data?.title}</div>
   </div>)
   
 }
